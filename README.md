da3a4b3a1ebd2fe4aa2f64f1acefda3f8590331d
Jakub <kubasosnicki@gmail.com>
2024-03-11 15:42:24 +0100

    Modified README.md and calc.py

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # hello-world 
-my first edit in repo
+my first edit in repo 2 files add at the same time
--- calc.py
+++ calc.py
@@ -2,3 +2,4 @@
 #kilometer to meter and miles calc
 liczba= int(input('Podaj ile km przeliczyć: '))
 print(f'{liczba} km to {liczba*1000} metrów oraz {liczba*0.6} mile lądowe')
+print(f'Koniec programu')
