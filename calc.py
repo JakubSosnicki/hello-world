981a9945207db136c2a8b843769eaf7735279477
Jakub <kubasosnicki@gmail.com>
2024-04-13 22:11:29 +0200

    Changed file calc.py for 3-way merge example part 3 for main

--- calc.py
+++ calc.py
@@ -1,5 +1,6 @@
 
 #kilometer to meter and miles calc
+#main changed 3-way merge test
 liczba= int(input('Podaj ile km przeliczyć: '))
 print(f'{liczba} km to {liczba*1000} metrów oraz {liczba*0.6} mile lądowe')
 print(f'Koniec programu')
