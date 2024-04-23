a3e7016c6251eeeafb981dd492adfb32120f3d6a
Jakub <kubasosnicki@gmail.com>
2024-04-23 18:52:01 +0200

    Changed print

--- calc.py
+++ calc.py
@@ -4,5 +4,5 @@
 liczba= int(input('Podaj ile km przeliczyć: '))
 print(f'{liczba} km to {liczba*1000} metrów oraz {liczba*0.6} mile lądowe')
 print(f'Koniec programu')
-print(f'feature-a')
+print(f'feature')
 
